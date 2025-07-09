1dce3b87e71824fc55a094904afec6bf65503fe1
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2025-07-09 19:04:56 +0530

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  assetPrefix: '/',
   /* config options here */
   images: {
     domains: ['res.cloudinary.com'],
